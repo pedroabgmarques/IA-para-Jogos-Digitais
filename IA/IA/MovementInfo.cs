4efc3b4b832d0f52677cce91aa96ee2d35def1b2
pedroabgmarques <pedroabgmarques@gmail.com>
2016-09-21 00:21:20 +0100

    Struct MovementInfo

---
+++ IA/IA/MovementInfo.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Informação necessária para representar inequivocamente um personagem no espaço 2 e 1/2D.
+ */
+
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IA
+{
+
+    /// <summary>
+    /// Informação necessária para representar inequivocamente um personagem no espaço 2 e 1/2D.
+    /// </summary>
+    public struct MovementInfo
+    {
+        Vector3 position; //current character position
+        Vector3 velocity; //current character velocity and direction
+        float orientation; //current character orientation
+        float rotation; //current character rotation speed
+    }
+}
