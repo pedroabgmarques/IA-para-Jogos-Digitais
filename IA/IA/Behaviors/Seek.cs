896fe281e3eff59748494d7b7873882027dd5f2b
pedroabgmarques <pedroabgmarques@gmail.com>
2016-09-21 00:47:58 +0100

    Classe abstrata Behavior; Behavior Seek

---
+++ IA/IA/Behaviors/Seek.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IA.Behaviors
+{
+
+    /// <summary>
+    /// Com este comportamento pretende-se que o personagem atinja determinada posição. Será calculada a 
+    /// direção em que se deve movimentar, e retornar um aceleração nessa direção. Será sótida em conta a 
+    /// aceleração máxima permitida.
+    /// Note-se que não há qualquer preocupação com uma aceleração ou desaceleração progressiva ao 
+    /// aproximar-se do destino.
+    /// </summary>
+    public class Seek : Behavior
+    {
+    }
+}
