3a090437662000319eb1d26777149ed810727099
pedroabgmarques <pedroabgmarques@gmail.com>
2016-09-21 23:23:20 +0100

    Behavior e demonstração Velocity

---
+++ IA/IA/Behaviors/Velocity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IA.Behaviors
+{
+
+    /// <summary>
+    /// Este movimento tenta imitar a velocidade (e direção do movimento) do alvo escolhido. 
+    /// É semelhante ao algoritmo do movimento de “Procura” mas em vez de tentar atingir a posição do alvo,
+    /// tentar-se-á atingir a sua velocidade.
+    /// </summary>
+    public class Velocity : Behavior
+    {
+
+        //origem do movimento
+        private MovementInfo origin;
+
+        //destino
+        private MovementInfo target;
+
+        //aceleração maxima
+        private float maxAcceleration;
+
+        //tempo para aceleração máxima
+        private float timeToTarget;
+
+        public Velocity(float maxAcceleration, float timeToTarget)
+        {
+            this.maxAcceleration = maxAcceleration;
+            this.timeToTarget = timeToTarget;
+        }
+
+        public Steering Update(
+            MovementInfo origin,
+            MovementInfo target)
+        {
+            this.origin = origin;
+            this.target = target;
+
+            return getSteering();
+        }
+
+        protected override Steering getSteering()
+        {
+            Steering steering = new Steering();
+
+            //não mexer se o alvo não se mexe
+            if (target.velocity.Length() == 0)
+            {
+                return Steering.None();
+            }
+
+            //acelerar e tentar obter a velicidade desejada
+            steering.linear = target.velocity - origin.velocity;
+            steering.linear /= timeToTarget;
+
+            //não acelerar demasiado
+            if (steering.linear.Length() > maxAcceleration)
+            {
+                steering.linear.Normalize();
+                steering.linear *= maxAcceleration;
+            }
+
+            steering.angular = 0;
+            
+            return steering;
+        }
+
+    }
+}
---
+++ IA/Velocity/NPC.cs
@@ -0,0 +1,82 @@
+﻿using IA;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Velocity
+{
+
+    /// <summary>
+    /// Define uma personagem controlada por IA
+    /// </summary>
+    public class NPC : MovableEntity
+    {
+
+        private MovableEntity target;
+
+        //Behaviors
+        IA.Behaviors.Velocity velocity;
+
+        public NPC(Random rnd, ContentManager content, GraphicsDevice graphics, MovableEntity target, float maxSpeed)
+        {
+
+            LoadContent(content);
+
+            //Behaviors
+            velocity = new IA.Behaviors.Velocity(0.1f, 2f);
+
+            //Inicializações
+            movementInfo.position = new Vector3(
+                rnd.Next(0, graphics.Viewport.Width - textura.Width),
+                0f,
+                rnd.Next(0, graphics.Viewport.Height - textura.Height));
+            movementInfo.orientation = 0f;
+            movementInfo.velocity = Vector3.Zero;
+            movementInfo.rotation = 0f;
+
+            origin = new Vector2(textura.Width / 2f, textura.Height / 2f);
+            this.maxSpeed = maxSpeed;
+            this.target = target;
+
+            steering = new Steering();
+        }
+
+        private void LoadContent(ContentManager content)
+        {
+            textura = content.Load<Texture2D>("soldadoNPC");
+        }
+
+        public void Update(GameTime gameTime, GraphicsDevice graphics)
+        {
+            steering = Steering.None();
+
+            //atualizar posição e orientação
+            movementInfo.position += movementInfo.velocity * gameTime.ElapsedGameTime.Milliseconds;
+            movementInfo.orientation += movementInfo.rotation * gameTime.ElapsedGameTime.Milliseconds;
+
+            //aplicar atrito
+            movementInfo.velocity *= 0.95f;
+            movementInfo.rotation *= 0.95f;
+
+            //calcular novo movimento
+            steering = velocity.Update(movementInfo, target.getMovementInfo());
+
+            movementInfo.velocity += steering.linear;
+            movementInfo.rotation += steering.angular;
+
+            //garantir velocidade máxima
+            if (movementInfo.velocity.Length() > maxSpeed)
+            {
+                movementInfo.velocity.Normalize();
+                movementInfo.velocity *= maxSpeed;
+            }
+
+            base.Update(graphics);
+
+        }
+    }
+}
